9b0ed1944d6e248e10aa96a45953fc4602e709a0
zhengrenkun <zhengrenkun@chandashi.com>
2022-09-28 14:21:08 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,23 @@
 #include <AsyncElegantOTA.h>
 #include <ESPAsyncWebServer.h>
 #include <ESP8266mDNS.h>
+#include <string.h>
 
 #include "blink.h"
 #include "lamp.h"
 #include "led.h"
+#include "PubSubClient.h"
 
 const char *apName = "light_wifi_config";
 const char *hostName = "mylight";
+
+#define ID_MQTT  "d6d8fcd160ce48a3b38ff76e7e2df726"     //用户私钥，控制台获取
+const char*  topic = "light002";        //主题名字，可在巴法云控制台自行创建，名称随意
+
+const char* mqtt_server = "bemfa.com"; //默认，MQTT服务器
+const int mqtt_server_port = 9501;      //默认，MQTT服务器
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 WiFiManager wm;
 AsyncWebServer server(80);
@@ -57,6 +67,49 @@ void start_server() {
 	Serial.println("HTTP server started");
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+	Serial.print("Topic:");
+	Serial.println(topic);
+	String msg = "";
+	for (unsigned int i = 0; i < length; i++) {
+		msg += (char)payload[i];
+	}
+	Serial.print("Msg:");
+	Serial.println(msg);
+	if (msg == "off") {
+		led_update_v(0);
+	} else if (msg == "on") {
+		led_update_v(100);
+	} else {
+		msg.replace("on#", "");
+		led_update_v(atoi(msg.c_str()));
+	}
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ID_MQTT)) {
+      Serial.println("connected");
+      Serial.print("subscribe:");
+      Serial.println(topic);
+      //订阅主题，如果需要订阅多个主题，可发送多条订阅指令client.subscribe(topic2);client.subscribe(topic3);
+      client.subscribe(topic);
+	  blink_ok();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+	  blink_err();
+      delay(5000);
+    }
+  }
+}
+
+
 bool shouldSaveConfig = false;
 void saveConfigCallback () {
   Serial.println("Should save config");
@@ -89,10 +142,16 @@ void setup() {
 		Serial.println(WiFi.localIP());
 		blink_ok();
 		start_server();
+		client.setServer(mqtt_server, mqtt_server_port); // 设置mqtt服务器
+  		client.setCallback(callback); // mqtt消息处理
 	}
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
 	MDNS.update();
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
 }
